5dd63fa9e8b382282b83d2cdc99d446a06216799
Pat Goebel <putterson@gmail.com>
2021-01-31 20:12:29 -0500

    Enable macro recording

--- keyboards/keebio/nyquist/keymaps/putterson/config.h
+++ keyboards/keebio/nyquist/keymaps/putterson/config.h
@@ -28,9 +28,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 #define EE_HANDS
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+// #undef RGBLED_NUM
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 12
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+
+//Disable creating recursive macros...they can lock up the keyboard
+#define DYNAMIC_MACRO_NO_NESTING
